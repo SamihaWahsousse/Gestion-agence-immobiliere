e9470e72b7911c4de6e1e1e3a5528284e5a10d52
Samiha Wahsousse <wahsousse.samiha@gmail.com>
2023-01-04 13:02:09 +0100

    init

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- ici je gère la taille de l'écran -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <!-- ici je met le titre de l'onglet de page -->
+    <title>Agence immobiliere SMImmo</title>
+
+    <!-- ici je met le link bootstrapp -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link href="asset/css/app.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- ici je fais mon responsive avec bootstrapp -->
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-sm-12 col-md-4 .col-lg-6">
+                One of three columns
+            </div>
+            <div class="col-sm-12 col-md-4 .col-lg-6">
+                One of three columns
+            </div>
+            <div class="col-sm-12 col-md-4 .col-lg-12">
+                One of three columns
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+<!-- je met le script javascript bootstrapp -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> -->
+
+</html>
