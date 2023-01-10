930296f914a870a2bdd4cddd45eff3d99c074a38
Samiha Wahsousse <wahsousse.samiha@gmail.com>
2023-01-10 09:58:19 +0100

    Add nav + immobilier cards

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- ici je gère la taille de l'écran -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <!-- ici je met le titre de l'onglet de page -->
+    <title>Agence immobiliere SMImmo</title>
+
+    <!-- ici je met le link bootstrapp -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="asset/css/app.min.css">
+
+    <script src="asset/js/app.js" defer></script>
+
+
+
+</head>
